684d6839841116bdaaa96908dd60c145a7c3be61
qualfoods <qualfoods@gmail.com>
2021-02-18 23:01:56 -0500

    first config delivery

---
+++ src/test/java/prueba/avla/com/ProductConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package prueba.avla.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
